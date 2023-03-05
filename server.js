784a1466f3f59e49b1621bc8737a8a7636107102
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2023-03-05 12:24:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
